1464661ca1312fb472c08b705c6a5611f4ec1055
Jeff Mosawy <jmosawy@gmail.com>
2017-09-11 20:01:31 +0430

    Postal code validation has been added into the library.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Phone = require('./phone');
+const Postal = require('./postal');
 
 const Verifier = function () {};
 
 Verifier.prototype.Phone = Phone;
+Verifier.prototype.Postal = Postal;
 
 module.exports = new Verifier();
