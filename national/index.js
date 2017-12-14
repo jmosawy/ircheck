7a3c4c75c5ccd77811aa43bed947f1a5ea7a4b05
Jeff Mosawy <jmosawy@gmail.com>
2017-12-14 19:49:10 +0330

    Company national code validation is added.

--- national/index.js
+++ national/index.js
@@ -23,4 +23,25 @@ National.prototype.isNationalCodeValid = function (nationalCode) {
   return Number(nationalCode.charAt(9)) === (11 - r);
 }
 
+/**
+* Check if the entered Iranian company national code is valid.
+* @param {string} companyNationalCode
+* @returns {boolean}
+*/
+National.prototype.isCompanyNationalCodeValid = function (companyNationalCode) {
+  if ((companyNationalCode.length !== 11) || (isNaN(Number(companyNationalCode)))) {
+    return false;
+  }
+
+  const ratios = [29, 27, 23, 19, 17, 29, 27, 23, 19, 17];
+  const baseNumber = Number(companyNationalCode.charAt(9)) + 2;
+  let sumOfMultipliedNumber = 0;
+  for (i = 0; i < 10; i += 1) {
+    sumOfMultipliedNumber += (Number(companyNationalCode.charAt(i)) + baseNumber) * ratios[i];
+}
+
+  const r = (sumOfMultipliedNumber % 11) === 10 ? 0 : (sumOfMultipliedNumber % 11);
+  return (r === Number(companyNationalCode.charAt(10)));
+}
+
 module.exports = new National();
--- test/national-test.js
+++ test/national-test.js
@@ -15,4 +15,17 @@ describe('National Tools', () => {
       expect(National.isNationalCodeValid('04120128431')).to.be.equal(false);
     });
   });
+
+  describe('isCompanyNationalCodeValid', () => {
+    it('should be valid company national code', () => {
+      expect(National.isCompanyNationalCodeValid('10380284790')).to.be.equal(true);
+      expect(National.isCompanyNationalCodeValid('14007259837')).to.be.equal(true);
+      expect(National.isCompanyNationalCodeValid('14007256420')).to.be.equal(true);
+    });
+    it('should not be valid company national code', () => {
+      expect(National.isCompanyNationalCodeValid('1038028479')).to.be.equal(false);
+      expect(National.isCompanyNationalCodeValid('14006259837')).to.be.equal(false);
+      expect(National.isCompanyNationalCodeValid('14007256429')).to.be.equal(false);
+    });
+  });
 });
